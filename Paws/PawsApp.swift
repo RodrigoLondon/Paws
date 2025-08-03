6a8285ff0c46d2a22784f71562f23c2ecd5fec58
RodrigoLondon <RodrigoLondon@users.noreply.github.com>
2025-08-03 15:39:10 +0100

    Initial Commit

---
+++ Paws/PawsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PawsApp.swift
+//  Paws
+//
+//  Created by Rodrigo Peña on 3/8/25.
+//
+
+import SwiftUI
+
+@main
+struct PawsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
